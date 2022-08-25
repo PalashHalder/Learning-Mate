3cc1b5748f2b61181210c2df54e5333f02095259
PalashHalder <halder.palash08@gmail.com>
2022-08-25 16:40:44 +0600

    This is a pdf book reader app

---
+++ app/src/main/java/info/palashhalder/learningmate/SplashActivity.java
@@ -0,0 +1,34 @@
+package info.palashhalder.learningmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+public class SplashActivity extends AppCompatActivity {
+
+    CountDownTimer countDownTimer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        countDownTimer = new CountDownTimer(5000,100) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(getApplicationContext(),(MainActivity.class));
+
+                startActivity(intent);
+                finish();
+            }
+        }.start();
+    }
+}
