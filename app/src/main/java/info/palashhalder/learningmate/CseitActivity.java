2b9c18c92ab758c248f71c372af0d5900f8166f9
PalashHalder <halder.palash08@gmail.com>
2023-01-04 15:41:08 +0600

    CseitActivity added

---
+++ app/src/main/java/info/palashhalder/learningmate/CseitActivity.java
@@ -0,0 +1,18 @@
+package info.palashhalder.learningmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class CseitActivity extends AppCompatActivity {
+
+    private Button btnDart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cseit);
+        
+    }
+}
--- app/src/main/java/info/palashhalder/learningmate/MainActivity.java
+++ app/src/main/java/info/palashhalder/learningmate/MainActivity.java
@@ -2,13 +2,29 @@ package info.palashhalder.learningmate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCse = findViewById(R.id.btnCse);
+
+        btnCse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, CseitActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
